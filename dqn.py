8599bf0d93a9d9e1d3ff1f2494e7e8186e09a0fb
Fortress94 <gabriel.roccabruna@studenti.unitn.it>
2019-05-29 11:45:40 +0200

    final version

--- dqn.py
+++ dqn.py
@@ -10,13 +10,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import re
-import pickle
-from torch.autograd import Variable
+import heapq
+
 #from tensorboardX import SummaryWriter
 
 from game.flappy_bird import GameState
-from maxHeap import MaxHeap
-from zipf import Quantile
 
 #writer = SummaryWriter()
 
@@ -28,16 +26,44 @@ class NeuralNetwork(nn.Module):
         self.gamma = 0.99
         self.final_epsilon = 0.0001
         self.initial_epsilon = 0.1
-        # Parameter for memory
-        self.alpha = 0.7
-        self.beta = 0.5
         self.number_of_iterations = 500000
-        #self.replay_memory_size = 20000
         self.replay_memory_size = 10000
-
         self.minibatch_size = 32
+        self.fc6 = None
+        
+	#IMAGE 54
+        if matrix_size == 54:
+            self.conv1 = nn.Conv2d(4, 32, 6, 2)
+            self.bn1 = nn.BatchNorm2d(32)
+            self.relu1 = nn.ReLU(inplace=True)
+            self.conv2 = nn.Conv2d(32, 64, 5, 2)
+            self.bn2 = nn.BatchNorm2d(64)
+            self.relu2 = nn.ReLU(inplace=True)
+            self.conv3 = nn.Conv2d(64, 64, 3, 2)
+            self.bn3 = nn.BatchNorm2d(64)
+            self.relu3 = nn.ReLU(inplace=True)
+            self.fc4 = nn.Linear(1600, 512)
+            self.relu4 = nn.ReLU(inplace=True)
+            self.fc5 = nn.Linear(512, self.number_of_actions)
+       
+        #IMAGE 64
+        if matrix_size == 64:
+                self.conv1 = nn.Conv2d(4, 32, 8, 2)
+                self.bn1 = nn.BatchNorm2d(32)
+                self.relu1 = nn.ReLU(inplace=True)
+                self.conv2 = nn.Conv2d(32, 64, 4, 2)
+                self.bn2 = nn.BatchNorm2d(64)
+                self.relu2 = nn.ReLU(inplace=True)
+                self.conv3 = nn.Conv2d(64, 64, 3, 1)
+                self.bn3 = nn.BatchNorm2d(64)
+                self.relu3 = nn.ReLU(inplace=True)
+                self.fc4 = nn.Linear(7744, 1232)
+                self.relu4 = nn.ReLU(inplace=True)
+                self.fc5 = nn.Linear(1232, 196)
+                self.relu5 = nn.ReLU(inplace=True)
+                self.fc6 = nn.Linear(196, self.number_of_actions)
 
-        #IMAGE 54
+	#IMAGE 75
         if matrix_size == 75:
             self.conv1 = nn.Conv2d(4, 32, 9, 3)
             self.bn1 = nn.BatchNorm2d(32)
@@ -51,7 +77,8 @@ class NeuralNetwork(nn.Module):
             self.fc4 = nn.Linear(1600, 512)
             self.relu4 = nn.ReLU(inplace=True)
             self.fc5 = nn.Linear(512, self.number_of_actions)
-	#IMAGE 84
+       
+        #IMAGE 84
         if matrix_size == 84:
             self.conv1 = nn.Conv2d(4, 32, 8, 4)
             self.bn1 = nn.BatchNorm2d(32)
@@ -66,7 +93,37 @@ class NeuralNetwork(nn.Module):
             self.relu4 = nn.ReLU(inplace=True)
             self.fc5 = nn.Linear(512, self.number_of_actions)
 
+	#IMAGE 87
+        if matrix_size == 87:
+            self.conv1 = nn.Conv2d(4, 32, 7, 4)
+            self.bn1 = nn.BatchNorm2d(32)
+            self.relu1 = nn.ReLU(inplace=True)
+            self.conv2 = nn.Conv2d(32, 64, 5, 2)
+            self.bn2 = nn.BatchNorm2d(64)
+            self.relu2 = nn.ReLU(inplace=True)
+            self.conv3 = nn.Conv2d(64, 64, 3, 2)
+            self.bn3 = nn.BatchNorm2d(64)
+            self.relu3 = nn.ReLU(inplace=True)
+            self.fc4 = nn.Linear(1024, 512)
+            self.relu4 = nn.ReLU(inplace=True)
+            self.fc5 = nn.Linear(512, self.number_of_actions)
+     
+ 	
 
+	#IMAGE 104
+        if matrix_size == 104:
+            self.conv1 = nn.Conv2d(4, 32, 9, 5)
+            self.bn1 = nn.BatchNorm2d(32)
+            self.relu1 = nn.ReLU(inplace=True)
+            self.conv2 = nn.Conv2d(32, 64, 4, 2)
+            self.bn2 = nn.BatchNorm2d(64)
+            self.relu2 = nn.ReLU(inplace=True)
+            self.conv3 = nn.Conv2d(64, 64, 3, 1)
+            self.bn3 = nn.BatchNorm2d(64)
+            self.relu3 = nn.ReLU(inplace=True)
+            self.fc4 = nn.Linear(3136, 512)
+            self.relu4 = nn.ReLU(inplace=True)
+            self.fc5 = nn.Linear(512, self.number_of_actions)
 
     def forward(self, x):
         out = self.conv1(x)
@@ -82,7 +139,8 @@ class NeuralNetwork(nn.Module):
         out = self.fc4(out)
         out = self.relu4(out)
         out = self.fc5(out)
-
+        if self.fc6 is not None:
+            out = self.fc6(self.relu5(out))
 
         return out
 
@@ -134,7 +192,7 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
         print()
 
 
-def train(model, start, filename, matrix_size, pid, weight_yes):
+def train(model, start, filename, matrix_size, pid):
     # define Adam optimizer
     optimizer = optim.Adam(model.parameters(), lr=1e-6)
 
@@ -146,6 +204,8 @@ def train(model, start, filename, matrix_size, pid, weight_yes):
 
     # initialize replay memory
     replay_memory = []
+    heap = []
+    heapq.heapify(heap)
 
     # initial action is do nothing
     action = torch.zeros([model.number_of_actions], dtype=torch.float32)
@@ -171,26 +231,7 @@ def train(model, start, filename, matrix_size, pid, weight_yes):
     # counter of the +1 reward
     reward_counter = 0
     # main infinite loop
-    state = state.cuda()
-    mem_id = 0
-    delete_id = 0
-    memory_map = {}
-    memory_content = {}
-    rank_heap = MaxHeap()
-    rank_heap.init_alpha(model.replay_memory_size, model.alpha)
-
-
-
-    delete_id = 0
-    # Get precomputed quatiles from file
-    N_list, quantiles = Quantile.load_quantiles()
-    current_quantile = 0
-    beta_annealing = (1 - model.beta) / model.number_of_iterations
-    beta = model.beta
-    sorted_mem = []
-    loss = None
-    loss_previous_state = torch.from_numpy(np.zeros([32], dtype=np.float32))
-
+    #state = state.cuda()
     while iteration < model.number_of_iterations:
         # get output from the neural network
         output = model(state)[0]
@@ -203,7 +244,8 @@ def train(model, start, filename, matrix_size, pid, weight_yes):
 
         # epsilon greedy exploration
         random_action = random.random() <= epsilon
-    
+        #if random_action:
+            #print("Performed random action!")
         action_index = [torch.randint(model.number_of_actions, torch.Size([]), dtype=torch.int)
                         if random_action
                         else torch.argmax(output)][0]
@@ -222,55 +264,18 @@ def train(model, start, filename, matrix_size, pid, weight_yes):
         action = action.unsqueeze(0)
         reward = torch.from_numpy(np.array([reward], dtype=np.float32)).unsqueeze(0)
 
-        # Add element to Hash maps for manage indixiziation problem
-        memory_map[mem_id] = (state, action, reward, state_1, terminal)
-        memory_content[(state, action, reward, state_1, terminal)] = mem_id
-        rank_heap.insert((mem_id, 1000))
-        mem_id = mem_id + 1
-
-        # Add new element to the heap with max td-error
-
+        # save transition to replay memory
+        replay_memory.append((state, action, reward, state_1, terminal))
 
         # if replay memory is full, remove the oldest transition
-        if len(memory_map) > model.replay_memory_size:
-            value = memory_map[delete_id]
-            del memory_content[value]
-            del memory_map[delete_id]
-            rank_heap.delete(delete_id)
-            delete_id = delete_id + 1
-            #replay_memory.pop(0)
+        if len(replay_memory) > model.replay_memory_size:
+            replay_memory.pop(0)
 
         # epsilon annealing
         epsilon = epsilon_decrements[iteration]
 
         # sample random minibatch
-        minibatch = []
-        ranking = []
-        batch_weights = []
-        # If the memory_map is lower than the first set of quantiles then it does uniform sample
-        if len(memory_map) < N_list[0]:
-            random_indexes = random.sample(range(0, mem_id), min(model.minibatch_size, len(memory_map)))
-            for i in random_indexes:
-                minibatch.append(memory_map[i])
-        else:
-            # Sample minibatch using a sorted array by TD-error
-            # and using quantiles as ranges
-            if iteration % 1000 == 0:
-                rank_heap.sort()
-                rank_heap.update_weights(beta)
-
-  
-            # Pool is a set of 32 elements(minibatch_size). An elment is composed by (memory_map index, td_error)
-            # Indexes are random numbers generated from each quantile. They correspond to the position of the sorted vector, so the ranking.
-            pool, indexes = Quantile.sample(model.minibatch_size, quantiles[current_quantile], rank_heap.lemon_tree)
-            for j in range(0, len(pool)):
-                ranking.append(rank_heap.ranking[indexes[j]]) # pj
-                batch_weights.append(rank_heap.weights[indexes[j]]) # wj
-                minibatch.append(memory_map[pool[j][0]])
-
-
-
-        #minibatch = random.sample(replay_memory, min(len(replay_memory), model.minibatch_size))
+        minibatch = random.sample(replay_memory, min(len(replay_memory), model.minibatch_size))
 
         # unpack minibatch
         state_batch = torch.cat(tuple(d[0] for d in minibatch))
@@ -294,7 +299,6 @@ def train(model, start, filename, matrix_size, pid, weight_yes):
                                   for i in range(len(minibatch))))
 
         # extract Q-value
-
         q_value = torch.sum(model(state_batch) * action_batch, dim=1)
 
         # PyTorch accumulates gradients by default, so they need to be reset in each pass
@@ -302,47 +306,16 @@ def train(model, start, filename, matrix_size, pid, weight_yes):
 
         # returns a new Tensor, detached from the current graph, the result will never require gradient
         y_batch = y_batch.detach()
-        # TD-error at step t
-        td_error = abs(y_batch - q_value)
-
-        delta = np.zeros(len(minibatch), dtype=np.float32)
-        for i in range(0, len(minibatch)):
-            element = minibatch[i]
-            element_td_error = td_error[i].item()
-            key = memory_content[element]
-            if iteration > N_list[0]:
-                normalizaition = None
-                max_weight = None
-                if iteration < model.replay_memory_size:
-                    normalizaition = rank_heap.normalizaition[iteration]
-                    max_weight = rank_heap.weights[iteration]
-                else:
-                    normalizaition = rank_heap.normalizaition[-1]
-                    max_weight = rank_heap.weights[-1]
-
-                p_i = ranking[i] / normalizaition # P(j) = pj / SUM pi
-                weight = batch_weights[i] / max_weight # wj / max wi
-                rank_heap.logical_update((key, element_td_error))# Update the priority
-                delta[i] = element_td_error * weight # Compute the delta
-               	rank_heap.update_all()
-
 
 
         # calculate loss
         loss = criterion(q_value, y_batch)
-        if iteration > N_list[0] and weight_yes:
-            delta = torch.from_numpy(np.array([delta], dtype=np.float32)).cuda()
-            loss = loss.squeeze() * delta
-            loss = loss.mean()
-            #print(loss)
-
+  
         # do backward pass
         loss.backward()
 
-        #loss.backward()
-
-
         optimizer.step()
+
 
         # set state to be state_1
         state = state_1
@@ -365,9 +338,6 @@ def train(model, start, filename, matrix_size, pid, weight_yes):
         if iteration % 500 == 0:
             outputlog.append('%s,%s,%s,%s,%s,%s\n' % (iteration, epsilon, qmax, loss.item(), reward_counter, global_max_wins))
             global_max_wins = 0
-            if iteration >= 1000 and iteration < model.replay_memory_size:
-                current_quantile = current_quantile + 1
-
             printProgressBar(iteration, model.number_of_iterations, prefix = 'Progress:', suffix = 'Complete', length = 100)
             with open(filename, 'a') as f:
                 while len(outputlog) > 0:
@@ -376,20 +346,6 @@ def train(model, start, filename, matrix_size, pid, weight_yes):
 
         if iteration % 250000 == 0:
             torch.save(model, "pretrained_model/" + str(matrix_size) + "_" + pid + "_current_model_" + str(iteration) + ".pth")
-
-
-        beta = beta + beta_annealing
-        '''
-            #writing live graph with tensorboard
-            #writer.add_scalar('data/epsilon', epsilon, iteration)
-            #writer.add_scalar('data/loss', loss, iteration)
-            #writer.add_scalar('data/q-max', np.max(output.cpu().detach().numpy()), iteration)
-            #writer.add_scalar('data/reward', reward.numpy()[0][0], iteration)
-
-            print("iteration:", iteration, "elapsed time:", time.time() - start, "epsilon:", epsilon, "action:",
-                  action_index.cpu().detach().numpy(), "reward:", reward.numpy()[0][0], "Q max:",
-                  np.max(output.cpu().detach().numpy()))
-         '''
 
 
 def test(model):
@@ -427,7 +383,7 @@ def test(model):
         state = state_1
 
 
-def main(mode, matrix_size, weight_yes):
+def main(mode, matrix_size):
     cuda_is_available = torch.cuda.is_available()
     matrix_size = int(matrix_size)
     if mode == 'test':
@@ -459,8 +415,10 @@ def main(mode, matrix_size, weight_yes):
 
         model.apply(init_weights)
         start = time.time()
-        train(model, start, filename, matrix_size, pid, weight_yes)
+        #pass also the file for logs
+        train(model, start, filename, matrix_size, pid)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(sys.argv[1], sys.argv[2])
+
